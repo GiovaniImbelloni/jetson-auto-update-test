cc283da6f42ca16567d0d7be98f05772e4e7b9e7
Giovani <giovani_simas@hotmail.com>
2025-03-12 15:06:55 +0000

    alteracão servico

--- servico_teste.cpp
+++ servico_teste.cpp
@@ -6,7 +6,7 @@
 int main() {
     std::ofstream log("/var/log/servico_teste.log", std::ios::app);
     while (true) {
-        log << "Rodando serviço..." << std::endl;
+        log << "Rodando serviço...2" << std::endl;
         log.flush();
         std::this_thread::sleep_for(std::chrono::seconds(5));
     }
