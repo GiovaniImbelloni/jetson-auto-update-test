18dd8664746ba7ec620cb220678b15d6b3a38f7d
Giovani <giovani_simas@hotmail.com>
2025-03-12 13:07:46 +0000

    Adicionando workflow para compilar e lançar releases

---
+++ servico_teste.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <thread>
+#include <chrono>
+
+int main() {
+    std::ofstream log("/var/log/servico_teste.log", std::ios::app);
+    while (true) {
+        log << "Rodando serviço..." << std::endl;
+        log.flush();
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+    }
+    return 0;
+}
+
