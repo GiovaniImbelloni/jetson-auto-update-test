cbbbb0796c3cbedff51ec66a473198b5dc81845e
Giovani Imbelloni <giovani_simas@hotmail.com>
2025-04-09 20:03:42 +0100

    change number of service test

--- servico_teste/servico_teste.cpp
+++ servico_teste/servico_teste.cpp
@@ -6,7 +6,7 @@
 int main() {
     std::ofstream log("/var/log/servico_teste.log", std::ios::app);
     while (true) {
-        log << "Rodando serviço...4" << std::endl;
+        log << "Rodando serviço...5" << std::endl;
         log.flush();
         std::this_thread::sleep_for(std::chrono::seconds(5));
     }
